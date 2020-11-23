2d01901720f014bd755226671cba5bc5118e4c6d
Alex Szabo <szabbo87@gmail.com>
2020-11-23 10:44:45 +0100

    FS-6 Create base Main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Register from "./pages/Register";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Route exact path="/" component={Main} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={SignIn} />
       </div>
