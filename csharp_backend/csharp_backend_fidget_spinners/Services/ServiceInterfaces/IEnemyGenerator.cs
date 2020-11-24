e33d76701f8a6fb808968385b816a2363f81ae0a
HaveaKillerDay <45069029+HaveaKillerDay@users.noreply.github.com>
2020-11-24 14:51:06 +0100

    Quickfix in interface

--- csharp_backend/csharp_backend_fidget_spinners/Services/ServiceInterfaces/IEnemyGenerator.cs
+++ csharp_backend/csharp_backend_fidget_spinners/Services/ServiceInterfaces/IEnemyGenerator.cs
@@ -1,14 +1,10 @@
 ﻿using csharp_backend_fidget_spinners.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace csharp_backend_fidget_spinners.Services.ServiceInterfaces
 {
     public interface IEnemyGenerator
     {
-        public Enemy GenerateEnemy(int myCharacterLevel);
+        public Enemy GenerateEnemy(Character myCharacterLevel);
         public int GenerateHealth(int myCharacterLevel);
         public int GenerateArmor(int myCharacterLevel);
     }
