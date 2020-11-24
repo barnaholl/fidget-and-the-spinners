381ef81d43d936a781d36eb8c7da4211737926c1
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-11-24 15:10:39 +0100

    create quest model and QuestGeneratorService

---
+++ csharp_backend/csharp_backend_fidget_spinners/Models/Quest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp_backend_fidget_spinners.Models
+{
+    public class Quest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TimeSpan QuestTime { get; set; }
+        public float EnergyCost { get; set; }
+        public float RewardCoin { get; set; }
+        public int RewardXP { get; set; }
+        //public Item RewardItem { get; set;}
+    }
+}
---
+++ csharp_backend/csharp_backend_fidget_spinners/Services/QuestGeneratorService.cs
@@ -0,0 +1,37 @@
+﻿using csharp_backend_fidget_spinners.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp_backend_fidget_spinners.Services
+{
+    public class QuestGeneratorService
+    {
+        private readonly Dictionary<string, string> quests;
+
+        public Quest GenerateQuest(Character character)
+        {
+            Random random = new Random();
+
+            Quest quest = new Quest
+            {
+
+            };
+
+            return quest;
+        }
+
+        public int GenerateCoinReward(int charlevel)
+        {
+            return 0;
+        }
+
+        public int GenerateXPReward(int charlevel)
+        {
+            return 0;
+        }
+
+
+    }
+}
