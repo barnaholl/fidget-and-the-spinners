90acfdaa72fcd47aa3f5a9d548e49dbe9ba72444
Eszter Bokkon <bokkone@gmail.com>
2020-11-25 23:44:41 +0100

    Add findByUsername and findAllPlayers methods to PlayerRepository

--- java_backend/user-handler-service/src/main/java/com/codecool/userhandlerservice/repository/PlayerRepository.java
+++ java_backend/user-handler-service/src/main/java/com/codecool/userhandlerservice/repository/PlayerRepository.java
@@ -2,8 +2,16 @@ package com.codecool.userhandlerservice.repository;
 
 import com.codecool.userhandlerservice.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface PlayerRepository extends JpaRepository<Player, String> {
+
+    Optional<Player> findByUserName(String s);
+
+    @Query("SELECT p FROM players AS p")
+    List<Player> findAllPlayers();
+
 }
