257dc0e51bf1c0088b4e4f92e4c1fdf39a1434be
barnaholl <hollbarna@freemail.hu>
2020-11-25 11:12:15 +0100

    create ItemFactoryService

---
+++ java_backend/item-handler-service/src/main/java/com/codecool/itemhandlerservice/service/ItemFactoryService.java
@@ -0,0 +1,12 @@
+package com.codecool.itemhandlerservice.service;
+
+import com.codecool.itemhandlerservice.repository.ItemRepository;
+
+public class ItemFactoryService {
+    private final ItemRepository itemRepository;
+
+    public ItemFactoryService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+}
