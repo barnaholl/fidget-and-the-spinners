12a97c585ea8366bd980887681e1213f5e0b570a
RolandOver9000 <csosz.roland2@gmail.com>
2020-11-25 12:37:21 +0100

    created equipment entity

---
+++ java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/model/Equipment.java
@@ -0,0 +1,23 @@
+package com.codecool.characterhandlerservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Equipment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
