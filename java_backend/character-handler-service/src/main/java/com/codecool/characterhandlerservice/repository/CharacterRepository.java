6eb7e639efcd29a22fbb22abc8c0c1dc30887f3b
RolandOver9000 <csosz.roland2@gmail.com>
2020-11-26 11:31:26 +0100

    created character repository

---
+++ java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/repository/CharacterRepository.java
@@ -0,0 +1,14 @@
+package com.codecool.characterhandlerservice.repository;
+
+import com.codecool.characterhandlerservice.model.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+
+    Optional<Character> getCharacterByUserId(Long userId);
+
+}
--- java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/service/CharacterService.java
+++ java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/service/CharacterService.java
@@ -1,5 +1,6 @@
 package com.codecool.characterhandlerservice.service;
 
+import com.codecool.characterhandlerservice.repository.CharacterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,10 +8,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CharacterService {
 
-
-
+    private final CharacterRepository characterRepository;
 
     public Character getCharacterByUserId(Long userId) {
-        return
+        return characterRepository.getCharacterByUserId(userId)
     }
 }
