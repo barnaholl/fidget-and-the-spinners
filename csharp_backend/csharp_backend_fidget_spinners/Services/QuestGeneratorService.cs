4dafa9f75797c7fe9597ae905868181527df5d1f
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-11-26 13:45:38 +0100

    test names and descriptions for questtexts

--- csharp_backend/csharp_backend_fidget_spinners/Services/QuestGeneratorService.cs
+++ csharp_backend/csharp_backend_fidget_spinners/Services/QuestGeneratorService.cs
@@ -8,13 +8,26 @@ namespace csharp_backend_fidget_spinners.Services
 {
     public class QuestGeneratorService
     {
+        /// <summary>
+        /// Properties
+        /// </summary>
+
         private Dictionary<string, string> questsTexts = new Dictionary<string, string>();
         private Random random = new Random();
         private readonly string[] questDifficulties = { "short", "medium", "long" };
+        private string[] questNames = { "test1", "test2", "test3" };
+        private string[] questDescriptions = { "desc1", "desc2", "desc3" };
 
+
+        /// <summary>
+        /// Generate a quest based on character level
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
 
         public Quest GenerateQuest(Character character)
         {
+            FillQuestTexts();
             string questDifficulty = questDifficulties[random.Next(0, questDifficulties.Length)];
             int questTextIndex = random.Next(0, questsTexts.Count);
             int timeAndEnergyCost = GenerateTimeAndEnergyCost(questDifficulty);
@@ -34,6 +47,13 @@ namespace csharp_backend_fidget_spinners.Services
             return quest;
         }
 
+        /// <summary>
+        /// Generate a random currency reward based on character level and quest difficulty
+        /// </summary>
+        /// <param name="charlevel"></param>
+        /// <param name="difficulty"></param>
+        /// <returns>A random integer</returns>
+
         public int GenerateCoinReward(int charlevel, string difficulty)
         {
             switch(difficulty)
@@ -52,6 +72,13 @@ namespace csharp_backend_fidget_spinners.Services
             }
         }
 
+        /// <summary>
+        /// Generate XP reward based on character level and quest difficulty
+        /// </summary>
+        /// <param name="charlevel"></param>
+        /// <param name="difficulty"></param>
+        /// <returns>Random integer</returns>
+
         public int GenerateXPReward(int charlevel, string difficulty)
         {
             switch(difficulty)
@@ -69,6 +96,12 @@ namespace csharp_backend_fidget_spinners.Services
                     return 0;
             }
         }
+
+        /// <summary>
+        /// Generate the time and energy cost based on quest difficulty
+        /// </summary>
+        /// <param name="difficulty"></param>
+        /// <returns>Random integer</returns>
 
         public int GenerateTimeAndEnergyCost(string difficulty)
         {
@@ -92,5 +125,13 @@ namespace csharp_backend_fidget_spinners.Services
         {
 
         }*/
+
+        private void FillQuestTexts()
+        {
+            for (int i = 0; i < questNames.Length -1; i++)
+            {
+                questsTexts.Add(questNames[i], questDescriptions[i]);
+            }
+        }
     }
 }
