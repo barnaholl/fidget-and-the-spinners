ea3c76e5663c440680f24aded8c46e9f8e7a96fa
RolandOver9000 <csosz.roland2@gmail.com>
2020-11-26 12:27:14 +0100

    created a service class for statistics

--- java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/service/CharacterService.java
+++ java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/service/CharacterService.java
@@ -1,6 +1,7 @@
 package com.codecool.characterhandlerservice.service;
 
 import com.codecool.characterhandlerservice.model.GameCharacter;
+import com.codecool.characterhandlerservice.model.Statistics;
 import com.codecool.characterhandlerservice.repository.CharacterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,16 @@ import javax.persistence.EntityNotFoundException;
 public class CharacterService {
 
     private final CharacterRepository characterRepository;
+    private final StatisticsService statisticsService;
 
     public GameCharacter getCharacterByUserId(Long userId) {
         return characterRepository
                 .getCharacterByUserId(userId)
                 .orElseThrow(() -> new EntityNotFoundException("Character not found by user id"));
+    }
+
+    public GameCharacter saveCharacter(Long userId) {
+        return characterRepository.save(GameCharacter.builder()
+        .characterStatistics(statisticsService.getStarterStatistics);
     }
 }
