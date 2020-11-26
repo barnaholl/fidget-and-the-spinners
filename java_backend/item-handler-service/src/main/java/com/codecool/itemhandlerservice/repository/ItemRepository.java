c3febebb1e8da41263b79efa689503d8dd5bec66
barnaholl <hollbarna@freemail.hu>
2020-11-26 12:22:30 +0100

    remove unnecessary database

--- java_backend/item-handler-service/src/main/java/com/codecool/itemhandlerservice/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.itemhandlerservice.repository;
-
-import com.codecool.itemhandlerservice.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item,Long> {
-}
