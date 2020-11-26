bf8ec0153f1fa90fe94a0eeaeb731455062bf543
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-11-26 13:06:13 +0100

    Add enemy to quest model, different quest difficulties

--- csharp_backend/csharp_backend_fidget_spinners/Models/Quest.cs
+++ csharp_backend/csharp_backend_fidget_spinners/Models/Quest.cs
@@ -11,8 +11,9 @@ namespace csharp_backend_fidget_spinners.Models
         public string Description { get; set; }
         public int QuestTime { get; set; }
         public float EnergyCost { get; set; }
-        public float RewardCoin { get; set; }
+        public int RewardCoin { get; set; }
         public int RewardXP { get; set; }
         //public Item RewardItem { get; set;}
+        public Enemy Enemy { get; set; }
     }
 }
--- csharp_backend/csharp_backend_fidget_spinners/Services/QuestGeneratorService.cs
+++ csharp_backend/csharp_backend_fidget_spinners/Services/QuestGeneratorService.cs
@@ -10,13 +10,14 @@ namespace csharp_backend_fidget_spinners.Services
     {
         private Dictionary<string, string> questsTexts = new Dictionary<string, string>();
         private Random random = new Random();
-
+        private readonly string[] questDifficulties = { "short", "medium", "long" };
 
 
         public Quest GenerateQuest(Character character)
         {
+            string questDifficulty = questDifficulties[random.Next(0, questDifficulties.Length)];
             int questTextIndex = random.Next(0, questsTexts.Count);
-            int timeAndEnergyCost = GenerateTimeAndEnergyCost();
+            int timeAndEnergyCost = GenerateTimeAndEnergyCost(questDifficulty);
 
             Quest quest = new Quest
             {
@@ -24,27 +25,67 @@ namespace csharp_backend_fidget_spinners.Services
                 Description = questsTexts.ElementAt(questTextIndex).Value,
                 QuestTime = timeAndEnergyCost,
                 EnergyCost = timeAndEnergyCost,
-                RewardCoin = GenerateCoinReward(character.CharacterLevel),
-                RewardXP = GenerateXPReward(character.CharacterLevel),
-                //RewardItem = GenerateRewardItem(character.CharacterLevel)
+                RewardCoin = GenerateCoinReward(character.CharacterLevel, questDifficulty),
+                RewardXP = GenerateXPReward(character.CharacterLevel, questDifficulty),
+                //RewardItem = GenerateRewardItem(character.CharacterLevel),
+                //Enemy = GenerateEnemy()
             };
 
             return quest;
         }
 
-        public float GenerateCoinReward(int charlevel)
+        public int GenerateCoinReward(int charlevel, string difficulty)
         {
-            return (float)Math.Round(random.NextDouble() * 100 + ((10 + charlevel) / 10), 2);
+            switch(difficulty)
+            {
+                case "short":
+                    return random.Next(10 + (charlevel * 2), (20 + (charlevel * 2)) + 1);
+
+                case "medium":
+                    return random.Next(20 + (charlevel * 2), (30 + (charlevel * 2)) + 1);
+
+                case "long":
+                    return random.Next(30 + (charlevel * 2), (40 + (charlevel * 2)) + 1);
+
+                default:
+                    return 0;
+            }
         }
 
-        public int GenerateXPReward(int charlevel)
+        public int GenerateXPReward(int charlevel, string difficulty)
         {
-            return random.Next(20 + (charlevel * 5), 30 + (charlevel * 5));
+            switch(difficulty)
+            {
+                case "short":
+                    return random.Next(20 + (charlevel * 2), (30 + (charlevel * 2)) + 1);
+
+                case "medium":
+                    return random.Next(30 + (charlevel * 2), (40 + (charlevel * 2)) + 1);
+
+                case "long":
+                    return random.Next(40 + (charlevel * 2), (50 + (charlevel * 2)) + 1);
+
+                default:
+                    return 0;
+            }
         }
 
-        public int GenerateTimeAndEnergyCost()
+        public int GenerateTimeAndEnergyCost(string difficulty)
         {
-            return random.Next(3, 7);
+            switch(difficulty)
+            {
+                case "short":
+                    return random.Next(1, 4);
+
+                case "medium":
+                    return random.Next(3, 7);
+
+                case "long":
+                    return random.Next(6, 10);
+
+                default:
+                    return 0;
+            }
         }
 
         /*public Item GenerateItemReward(int charlevel)
