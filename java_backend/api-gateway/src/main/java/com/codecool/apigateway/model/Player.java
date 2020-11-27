cbfa491d9cde93d492adbdc6c5e28ab889fe6fa1
Eszter Bokkon <bokkone@gmail.com>
2020-11-27 07:42:13 +0100

    Create models Player and UserCredentials

---
+++ java_backend/api-gateway/src/main/java/com/codecool/apigateway/model/Player.java
@@ -0,0 +1,19 @@
+package com.codecool.apigateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Player {
+
+    private String userName;
+    private String hashedPassword;
+    private Set<String> roles;
+}
