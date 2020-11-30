e499e5348b2ae273e5ed753669afe993eeac9ce1
barnaholl <hollbarna@freemail.hu>
2020-11-30 19:14:16 +0100

    getRandomItemRarityIsInEnum()

--- java_backend/item-handler-service/src/test/java/com/codecool/itemhandlerservice/ItemFactoryServiceUnitTests.java
+++ java_backend/item-handler-service/src/test/java/com/codecool/itemhandlerservice/ItemFactoryServiceUnitTests.java
@@ -2,12 +2,14 @@ package com.codecool.itemhandlerservice;
 
 import com.codecool.itemhandlerservice.entity.Item;
 import com.codecool.itemhandlerservice.model.ComputerName;
+import com.codecool.itemhandlerservice.model.Rarity;
 import com.codecool.itemhandlerservice.service.ItemFactoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -57,4 +59,51 @@ public class ItemFactoryServiceUnitTests {
 
         assertThat(item.getName()).isIn(names);
     }
+
+    @Test
+    void getRandomItemEquipmentSlotIsNotNull(){
+        Item item=itemFactoryService.getRandomItem(1L);
+        assertThat(item.getEquipmentSlot()).isNotNull();
+    }
+    @Test
+    void getRandomItemEquipmentSlotIsInEnum(){
+        Item item=itemFactoryService.getRandomItem(1L);
+
+        List<String> equipmentSlots = Stream.of(ComputerName.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+
+        assertThat(item.getName()).isIn(equipmentSlots);
+    }
+
+    @Test
+    void getRandomItemItemLevelIsNotNull(){
+        Item item=itemFactoryService.getRandomItem(1L);
+        assertThat(item.getItemLevel()).isNotNull();
+    }
+
+    @Test
+    void getRandomItemItemLevelIsEqualsParam(){
+        Long param=1L;
+        Item item=itemFactoryService.getRandomItem(param);
+        assertThat(item.getItemLevel()).isEqualTo(param);
+    }
+
+    @Test
+    void getRandomItemRarityIsNotNull(){
+        Item item=itemFactoryService.getRandomItem(1L);
+        assertThat(item.getRarity()).isNotNull();
+    }
+
+    @Test
+    void getRandomItemRarityIsInEnum(){
+        Item item=itemFactoryService.getRandomItem(1L);
+
+        List<Rarity> rarity = Arrays.asList(Rarity.values());
+
+        assertThat(item.getRarity()).isIn(rarity);
+    }
+
+
+
 }
