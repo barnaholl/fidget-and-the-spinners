b0cb60735f3b73d1e60f12e833dd189b4d927ad2
Paebo <petibors2@gmail.com>
2020-11-30 13:43:03 +0100

    FS-6 users land on the landing page when opening the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Main from "./pages/Main";
 import CharacterCreation from "./pages/CharacterCreation";
 import "./App.css";
 import Landing from "./pages/Landing";
@@ -12,10 +11,9 @@ function App() {
     <Router>
       <div className="background-image"></div>
       <div className="App">
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Register} />
         <Route path="/login" component={Login} />
-        <Route path="/landing" component={Landing} />
         <Route
           exact
           path="/username/character-creation"
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -6,17 +6,18 @@ import Button from "@material-ui/core/Button";
 function Landing() {
     return (
         <div align="center">
-            <h3>Welcome Traveler!</h3>
-            <p>Beyond this point you will find adventures, success and failures. </p>
-            <p>At some point you might wish you had never started it.</p> 
-            <p>But you know deep down that in the end, it's all worth it.</p>
-            <Link to={"/username/character-creation"}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-            >
-              Continue
+          <h3>Welcome Traveler!</h3>
+          <p>Beyond this point you will find adventures, success and failures. </p>
+          <p>At some point you might wish you had never started it.</p> 
+          <p>But you know deep down that in the end, it's all worth it.</p>
+          <Link to={"/login"}>
+            <Button variant="contained" color="primary" class="landing-button">
+              Login
+            </Button>
+          </Link>
+          <Link to={"/register"}>
+            <Button variant="contained" color="primary" class="landing-button">
+              Register
             </Button>
           </Link>
         </div>
