7e87c804ce29929ee85d4edeaf73a1732b8ea5b9
barnaholl <hollbarna@freemail.hu>
2020-12-01 15:02:09 +0100

    getItemByLevelSuccessfulStatus

--- java_backend/item-handler-service/src/test/java/com/codecool/itemhandlerservice/ItemFactoryControllerTest.java
+++ java_backend/item-handler-service/src/test/java/com/codecool/itemhandlerservice/ItemFactoryControllerTest.java
@@ -1,20 +1,43 @@
 package com.codecool.itemhandlerservice;
 
 import com.codecool.itemhandlerservice.controller.ItemController;
+import com.codecool.itemhandlerservice.entity.Item;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ItemFactoryControllerTest {
+
+    private MockMvc mockMvc;
 
     @Autowired
     private ItemController itemController;
 
     @Test
-    public void contextLoads() throws Exception {
-        assertThat(itemController).isNotNull();
+    void getItemByLevelSuccessfulStatus() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/1");
+        mockMvc = MockMvcBuilders.standaloneSetup(itemController).build();
+        mockMvc.perform(request).andExpect(status().is2xxSuccessful());
     }
+
+
 }
