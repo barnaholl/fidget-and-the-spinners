e8bdfebef97267cfe127402c4c97cdda1ca4bdd7
Paebo <petibors2@gmail.com>
2020-12-01 11:14:14 +0100

    Remove link from footer

--- frontend/src/components/Copyright.js
+++ frontend/src/components/Copyright.js
@@ -1,13 +1,12 @@
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 import "../App.css";
 
 export default function Copyright() {
   return (
     <footer>
       <Typography variant="title" color="textSecondary" align="center">
-        {"Copyright © "}
-        <Link color="inherit">Fidget and the Spinners</Link>{" "}
+        {"Copyright © Fidget and the Spinners"}
+        {" "}
         {new Date().getFullYear()}
         {"."}
       </Typography>
