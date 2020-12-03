3ac4a67ee4e7e22b56ce208be03f6660bddee434
barnaholl <hollbarna@freemail.hu>
2020-12-03 09:54:54 +0100

    ShopControllerTest

---
+++ java_backend/shop-handler-service/src/test/java/com/codecool/shophandlerservice/ShopControllerTest.java
@@ -0,0 +1,12 @@
+package com.codecool.shophandlerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShopControllerTest {
+    @Test
+    public void test(){
+
+    }
+}
