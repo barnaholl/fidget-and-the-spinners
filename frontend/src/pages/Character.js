28902909e4fc974bfeaf715ccd0744382441105f
RayToth <50377331+RayToth@users.noreply.github.com>
2020-12-03 11:47:17 +0100

    Create default equipment icon

--- frontend/src/pages/Character.js
+++ frontend/src/pages/Character.js
@@ -3,10 +3,21 @@ import Navbar from "../layout/Navbar";
 import Stats from "../components/Stats";
 import Inventory from "../components/Inventory";
 import Grid from "@material-ui/core/Grid";
+import CharacterEquipment from "../components/CharacterEquipment";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  grid: {
+    marginTop: 50,
+  },
+});
 
 export default function Character() {
+  const classes = useStyles();
+
   return (
     <Grid
+      className={classes.grid}
       container
       direction="row"
       justify="space-evenly"
@@ -15,6 +26,9 @@ export default function Character() {
     >
       <Grid item xs={2}>
         <Navbar />
+      </Grid>
+      <Grid item xs={5}>
+        <CharacterEquipment />
       </Grid>
       <Grid item xs={5}>
         <Stats />
