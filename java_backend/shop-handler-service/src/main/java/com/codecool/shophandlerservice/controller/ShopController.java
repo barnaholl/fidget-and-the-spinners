66bbfce32b6d32e5ba397769d2d937b9ef9523ef
barnaholl <hollbarna@freemail.hu>
2020-12-03 12:00:29 +0100

    ShopController/getItemsByCharacterId

--- java_backend/shop-handler-service/src/main/java/com/codecool/shophandlerservice/controller/ShopController.java
+++ java_backend/shop-handler-service/src/main/java/com/codecool/shophandlerservice/controller/ShopController.java
@@ -1,9 +1,13 @@
 package com.codecool.shophandlerservice.controller;
 
+import com.codecool.shophandlerservice.entity.Item;
 import com.codecool.shophandlerservice.service.ShopService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ShopController {
@@ -17,5 +21,10 @@ public class ShopController {
     @PostMapping("/{characterId}/{characterLevel}")
     public void getNewItemByCharacterIdAndCharacterLevel(@PathVariable("characterId") Long characterId,@PathVariable("characterLevel") Long characterLevel){
         shopService.getNewItemByCharacterIdAndCharacterLevel(characterId,characterLevel);
+    }
+
+    @GetMapping("/{characterId}")
+    public List<Item> getItemsByCharacterId(@PathVariable("characterId") Long characterId){
+        return shopService.getItemsByCharacterId(characterId);
     }
 }
