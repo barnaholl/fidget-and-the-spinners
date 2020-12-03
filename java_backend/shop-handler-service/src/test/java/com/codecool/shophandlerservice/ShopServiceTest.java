76a8a44a55f0174beeddf45c6ba951874837fd6f
barnaholl <hollbarna@freemail.hu>
2020-12-03 10:24:55 +0100

    getNewItemByCharacterIdAndCharacterLevelIsSaving()

--- java_backend/shop-handler-service/src/test/java/com/codecool/shophandlerservice/ShopServiceTest.java
+++ java_backend/shop-handler-service/src/test/java/com/codecool/shophandlerservice/ShopServiceTest.java
@@ -1,13 +1,28 @@
 package com.codecool.shophandlerservice;
 
+import com.codecool.shophandlerservice.repository.ItemRepository;
+import com.codecool.shophandlerservice.service.ShopService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ShopServiceTest {
 
+	@Autowired
+	private ShopService shopService;
+
+	@Autowired
+	private ItemRepository itemRepository;
+
 	@Test
-	void contextLoads() {
+	void getNewItemByCharacterIdAndCharacterLevelIsSaving() {
+		shopService.getNewItemByCharacterIdAndCharacterLevel(1,10);
+		int size=itemRepository.findAll().size();
+
+		assertThat(size).isEqualTo(1);
 	}
 
 }
