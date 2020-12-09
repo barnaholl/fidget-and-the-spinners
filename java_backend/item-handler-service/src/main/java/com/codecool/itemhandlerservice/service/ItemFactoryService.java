090c6e223bbb3251332a2144acea384c0ae533b6
barnaholl <hollbarna@freemail.hu>
2020-12-09 13:27:42 +0100

    ItemFactoryService Constants refactored with enums

--- java_backend/item-handler-service/src/main/java/com/codecool/itemhandlerservice/service/ItemFactoryService.java
+++ java_backend/item-handler-service/src/main/java/com/codecool/itemhandlerservice/service/ItemFactoryService.java
@@ -11,24 +11,9 @@ public class ItemFactoryService {
 
     private static final int NUMBER_OF_BASE_STATS=5;
 
-    private static final int EPIC_ITEM_SELL_PRICE = 4;
-    private static final int RARE_ITEM_SELL_PRICE = 3;
-    private static final int UNCOMMON_ITEM_SELL_PRICE = 2;
-    private static final int COMMON_ITEM_SELL_PRICE = 1;
-
-    private static final int EPIC_ITEM_BUY_PRICE = 16;
-    private static final int RARE_ITEM_BUY_PRICE = 9;
-    private static final int UNCOMMON_ITEM_BUY_PRICE = 4;
-    private static final int COMMON_ITEM_BUY_PRICE = 2;
-
-    private static final int EPIC_ITEM_STAT_MULTIPLIER=4;
-    private static final int RARE_ITEM_STAT_MULTIPLIER = 3;
-    private static final int UNCOMMON_ITEM_STAT_MULTIPLIER = 2;
-    private static final int COMMON_ITEM_STAT_MULTIPLIER = 1;
     private static final int MOTIVATION_BY_LEVEL = 2;
 
-
-    public List<Item> getMultipleRandomItemsByPlayerLevel(Long playerLevel,int numberOfItems){
+    public List<Item> getMultipleRandomItemsByPlayerLevel(Long playerLevel, int numberOfItems){
         if (playerLevel <= 0 || numberOfItems <= 0) {
             throw new IllegalArgumentException("Parameters should be positive");
         }
@@ -103,32 +88,17 @@ public class ItemFactoryService {
         return itemLevel*MOTIVATION_BY_LEVEL;
     }
 
+
     private Long getBuyPrice(Long itemLevel,Rarity rarity) {
-        switch(rarity) {
-            case EPIC:
-                return itemLevel*EPIC_ITEM_BUY_PRICE;
-            case RARE:
-                return itemLevel*RARE_ITEM_BUY_PRICE;
-            case UNCOMMON:
-                return itemLevel*UNCOMMON_ITEM_BUY_PRICE;
-            case COMMON:
-                return itemLevel*COMMON_ITEM_BUY_PRICE;
-        }
-        return null;
+        return rarity.getMap().get("buyPrice")*itemLevel;
     }
 
     private Long getSellPrice(Long itemLevel,Rarity rarity) {
-        switch(rarity) {
-            case EPIC:
-                return itemLevel*EPIC_ITEM_SELL_PRICE;
-            case RARE:
-                return itemLevel*RARE_ITEM_SELL_PRICE;
-            case UNCOMMON:
-                return itemLevel*UNCOMMON_ITEM_SELL_PRICE;
-            case COMMON:
-                return itemLevel*COMMON_ITEM_SELL_PRICE;
-        }
-        return null;
+        return rarity.getMap().get("sellPrice")*itemLevel;
+    }
+
+    private Long setOverallStats(Long itemLevel, Rarity rarity) {
+        return rarity.getMap().get("statMultiplier")*itemLevel;
     }
 
 
@@ -150,19 +120,7 @@ public class ItemFactoryService {
         return stats;
     }
 
-    private Long setOverallStats(Long itemLevel, Rarity rarity) {
-        switch(rarity) {
-            case EPIC:
-                return itemLevel*EPIC_ITEM_STAT_MULTIPLIER;
-            case RARE:
-                return itemLevel*RARE_ITEM_STAT_MULTIPLIER;
-            case UNCOMMON:
-                return itemLevel*UNCOMMON_ITEM_STAT_MULTIPLIER;
-            case COMMON:
-                return itemLevel*COMMON_ITEM_STAT_MULTIPLIER;
-        }
-        return null;
-    }
+
 
     private Rarity getRandomRarity() {
         Random random=new Random();
