b73ca59d9c7613161cdadcbf084cec7bacb0a87f
Paebo <petibors2@gmail.com>
2020-12-10 10:53:31 +0100

    create container for characterwindow

--- frontend/src/components/CharacterWindow.js
+++ frontend/src/components/CharacterWindow.js
@@ -5,21 +5,28 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import Stats from "./Stats";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
   table: {
     maxWidth: 650,
   },
-});
+}));
 
 export default function CharacterWindow() {
   const classes = useStyles();
 
   return (
-    <>
+    <Container className={classes.root}>
       <Stats />
-    </>
+    </Container>
   );
 }
