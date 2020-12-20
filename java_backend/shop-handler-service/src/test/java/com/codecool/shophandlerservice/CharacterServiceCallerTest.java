0841261c8662e9ef601cc1eec882f705cc43ff0d
barnaholl <hollbarna@freemail.hu>
2020-12-20 12:28:30 +0100

    CharacterServiceCallerTest

---
+++ java_backend/shop-handler-service/src/test/java/com/codecool/shophandlerservice/CharacterServiceCallerTest.java
@@ -0,0 +1,19 @@
+package com.codecool.shophandlerservice;
+
+import com.codecool.shophandlerservice.entity.Item;
+import com.codecool.shophandlerservice.service.CharacterServiceCaller;
+import com.codecool.shophandlerservice.service.ItemServiceCaller;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class CharacterServiceCallerTest {
+
+    @Autowired
+    private CharacterServiceCaller characterServiceCaller;
+
+    //TODO after having characters
+}
--- java_backend/shop-handler-service/src/test/java/com/codecool/shophandlerservice/ItemServiceCallerTest.java
+++ java_backend/shop-handler-service/src/test/java/com/codecool/shophandlerservice/ItemServiceCallerTest.java
@@ -2,6 +2,7 @@ package com.codecool.shophandlerservice;
 
 import com.codecool.shophandlerservice.entity.Item;
 import com.codecool.shophandlerservice.service.ItemServiceCaller;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
