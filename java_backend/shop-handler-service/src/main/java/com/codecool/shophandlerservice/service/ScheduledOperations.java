2f6c5b2a51ad8b70f9981b6731257610d7029944
barnaholl <hollbarna@freemail.hu>
2021-01-04 13:06:27 +0100

    ScheduledOperations set refresh to every hour

--- java_backend/shop-handler-service/src/main/java/com/codecool/shophandlerservice/service/ScheduledOperations.java
+++ java_backend/shop-handler-service/src/main/java/com/codecool/shophandlerservice/service/ScheduledOperations.java
@@ -19,14 +19,12 @@ public class ScheduledOperations {
     }
 
     //@Scheduled(cron = "0 0 0 * * ?") //Every day at "0:00"
-    //@Scheduled(cron = "0 0 * * * ?") //Every hour
-    @Scheduled(cron = "0 */5 * * * ?") //Every 5 minute
+    @Scheduled(cron = "0 0 * * * ?") //Every hour
     //@Scheduled(cron = "0 * * * * ?") //Every minute
     private void refreshShops() throws Exception {
         Map<String,Integer> characterLevels=characterServiceCaller.getCharacterLevels();
         for (String key:characterLevels.keySet()) {
             shopService.refreshItemsByCharacterId(Long.valueOf(key),(long)characterLevels.get(key));
         }
-
     }
 }
