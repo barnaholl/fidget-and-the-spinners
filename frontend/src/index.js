1d9df2a7b956829d47abf4ef0276e16c138f3dfd
RayToth <50377331+RayToth@users.noreply.github.com>
2021-01-06 15:24:41 +0100

    Import index.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
 // import Copyright from "./components/Copyright";
 import reportWebVitals from "./reportWebVitals";
 
