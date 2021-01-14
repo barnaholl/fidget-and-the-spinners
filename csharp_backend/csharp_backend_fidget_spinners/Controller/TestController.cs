c3131c14f83963cb862503a2a55b1fc479ed9412
Olic <galoliver05@gmail.com>
2021-01-14 10:51:14 +0100

    created a controller for testing purposes

---
+++ csharp_backend/csharp_backend_fidget_spinners/Controller/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace csharp_backend_fidget_spinners.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult TestEndpoint(string id)
+        {
+            return Ok();
+        }
+    }
+
+}
