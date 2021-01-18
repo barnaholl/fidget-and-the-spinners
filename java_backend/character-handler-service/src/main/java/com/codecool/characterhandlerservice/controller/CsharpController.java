99eedcc9cbf344df5dc6ce8cd6c074b0ec67ef3c
RolandOver9000 <csosz.roland2@gmail.com>
2021-01-18 17:20:31 +0100

    C#service tested through gateway successfuly

--- java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/controller/CsharpController.java
+++ java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/controller/CsharpController.java
@@ -3,12 +3,16 @@ package com.codecool.characterhandlerservice.controller;
 
 import com.codecool.characterhandlerservice.service.CsharpCallerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/csharp")
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/csharp")
+@Slf4j
 public class CsharpController {
 
     private final CsharpCallerService csharpCallerService;
