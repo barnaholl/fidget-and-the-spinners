8160c04c2ec04bdec447c2c1a62cd16e95e63ae7
barnaholl <hollbarna@freemail.hu>
2021-01-18 11:40:18 +0100

    update Inventory

--- java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/service/InventoryService.java
+++ java_backend/character-handler-service/src/main/java/com/codecool/characterhandlerservice/service/InventoryService.java
@@ -2,17 +2,24 @@ package com.codecool.characterhandlerservice.service;
 
 import com.codecool.characterhandlerservice.model.GameCharacter;
 import com.codecool.characterhandlerservice.model.Inventory;
+import com.codecool.characterhandlerservice.model.Item;
 import com.codecool.characterhandlerservice.repository.CharacterRepository;
+import com.codecool.characterhandlerservice.repository.InventoryRepository;
+import com.codecool.characterhandlerservice.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
 
     private final CharacterRepository characterRepository;
+
+    private final ItemRepository itemRepository;
 
     public Inventory getNewInventory() {
         return Inventory.builder().build();
@@ -27,7 +34,15 @@ public class InventoryService {
         GameCharacter gameCharacter = characterRepository.findById(characterId)
                 .orElseThrow(()->new EntityNotFoundException());
 
-        gameCharacter.setCharacterInventory(newInventory);
+        Inventory inventory=gameCharacter.getCharacterInventory();
+        List<Item> newItems= new ArrayList<>();
+
+        for(Item newItem:newInventory.getInventoryItems()){
+            newItem.setInventory(inventory);
+            itemRepository.save(newItem);
+            newItems.add(newItem);
+        }
+        inventory.setInventoryItems(newItems);
 
         characterRepository.save(gameCharacter);
     }
