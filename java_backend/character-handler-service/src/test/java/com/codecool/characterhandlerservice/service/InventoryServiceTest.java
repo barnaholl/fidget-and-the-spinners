95c0841b604843df71232249964374ee7719f510
RolandOver9000 <csosz.roland2@gmail.com>
2021-01-19 17:47:31 +0100

    created getInventoryByCharacterIdTest method for inventory service

--- java_backend/character-handler-service/src/test/java/com/codecool/characterhandlerservice/service/InventoryServiceTest.java
+++ java_backend/character-handler-service/src/test/java/com/codecool/characterhandlerservice/service/InventoryServiceTest.java
@@ -1,12 +1,38 @@
 package com.codecool.characterhandlerservice.service;
 
+import com.codecool.characterhandlerservice.model.GameCharacter;
+import com.codecool.characterhandlerservice.repository.CharacterRepository;
+import com.codecool.characterhandlerservice.repository.ItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
+@Import({InventoryService.class})
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class InventoryServiceTest {
 
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @Test
+    public void getInventoryByCharacterIdTest() {
+        GameCharacter gameCharacter = new GameCharacter();
+        Long savedId = characterRepository.save(gameCharacter).getId();
+
+        assertThat(inventoryService.getInventoryByCharacterId(savedId)
+                .equals(gameCharacter.getCharacterInventory()));
+    }
 }
