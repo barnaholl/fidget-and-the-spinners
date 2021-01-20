ec5edad99d1c5e7b1162a88da39c052277ec8676
RolandOver9000 <csosz.roland2@gmail.com>
2021-01-20 11:27:29 +0100

    generated CharacterMother class for tests

---
+++ java_backend/character-handler-service/src/test/java/com/codecool/characterhandlerservice/utility/CharacterMother.java
@@ -0,0 +1,25 @@
+package com.codecool.characterhandlerservice.utility;
+
+import com.codecool.characterhandlerservice.model.GameCharacter;
+
+import static com.codecool.characterhandlerservice.model.CharacterClass.TESTER;
+
+public class CharacterMother {
+
+    public static GameCharacter getTesterCharacterWithEquipmentAndInventoryItems() {
+        GameCharacter newGameCharacter = GameCharacter.builder()
+                .characterLevel(10)
+                .characterClass(TESTER)
+                .characterCurrency((long) 1000)
+                .characterName("TesterCharacter")
+                .energyLevel(2000)
+                .characterExperience((long) 2000)
+                .characterInventory()
+                .characterEquipment()
+                .build();
+
+        newGameCharacter.setCharacterStatistics(CharacterStatisticsMother.getCharacterStatistics(newGameCharacter));
+
+    }
+
+}
