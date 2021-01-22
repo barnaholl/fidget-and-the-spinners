0bc34660fc98361627fe0fa74be6c83a3bc48c8d
RolandOver9000 <csosz.roland2@gmail.com>
2021-01-22 13:25:21 +0100

    implemented swagger for java services

--- java_backend/shop-handler-service/src/main/java/com/codecool/shophandlerservice/ShopHandlerServiceApplication.java
+++ java_backend/shop-handler-service/src/main/java/com/codecool/shophandlerservice/ShopHandlerServiceApplication.java
@@ -7,10 +7,16 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableScheduling
+@EnableSwagger2
 public class ShopHandlerServiceApplication {
 
 	public static void main(String[] args) {
@@ -21,5 +27,14 @@ public class ShopHandlerServiceApplication {
 	//@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.ant("/**"))
+				.build();
 	}
 }
