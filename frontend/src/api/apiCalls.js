024262da285f9dde26f4a826f0aa793bb1c7306b
RayToth <50377331+RayToth@users.noreply.github.com>
2021-01-26 14:08:18 +0100

    Update environment variables

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -1,21 +1,27 @@
 import axios from "axios";
 
 export const fetchStatsApi = async () => {
-  return await axios.get("http://localhost:5000/stats/1");
+  return await axios.get(
+    `${process.env.REACT_APP_GATEWAY_API_URL}5000/stats/1`
+  );
 };
 
 export const fetchQuestsApi = async () => {
-  return await axios.get("http://localhost:5000/quests");
+  return await axios.get(`${process.env.REACT_APP_GATEWAY_API_URL}5000/quests`);
 };
 
 export const fetchInventoryItemsApi = async () => {
-  return await axios.get("http://localhost:5000/inventory");
+  return await axios.get(
+    `${process.env.REACT_APP_GATEWAY_API_URL}5000/inventory`
+  );
 };
 
 export const fetchEquipmentItemsApi = async () => {
-  return await axios.get("http://localhost:5000/equipment");
+  return await axios.get(
+    `${process.env.REACT_APP_GATEWAY_API_URL}5000/equipment`
+  );
 };
 
 export const fetchShopItemsApi = async () => {
-  return await axios.get("http://localhost:5000/shop");
+  return await axios.get(`${process.env.REACT_APP_GATEWAY_API_URL}5000/shop`);
 };
